a680bde8cc0561e28d575a9384986547db8b8b68
kasarmiento <kasarmiento@csupomona.edu>
2015-02-11 09:08:53 -0800

    Completely revamped GUI, filters for #1

---
+++ src/edu/cpp/cs555/hw/ImageProcessor.java
@@ -0,0 +1,412 @@
+package edu.cpp.cs555.hw;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ImageProcessor {
+	
+	static int n;
+	static int bdr;
+	static int imageHeight;
+	static int imageWidth;
+	static BufferedImage image, backup;
+	
+	public ImageProcessor(BufferedImage img) {
+		image = img;
+		backup = img;
+		imageHeight = image.getHeight();
+		imageWidth = image.getWidth();
+		
+	}
+	
+	public void setFilterSize(int filterSize) {
+		n = filterSize;
+		bdr = n/2;
+	}
+	
+	private static BufferedImage smoothAndCrop(BufferedImage result) {
+		
+		int ii = 0;
+		int jj = 0;
+		BufferedImage newResult = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_BYTE_GRAY);
+		
+		int sum = 0;
+		int avg = 0;
+		
+		int resultHeight = result.getHeight();
+		int resultWidth = result.getWidth();
+		
+		for(int i = bdr; i < resultHeight-bdr; i++) {
+			for(int j = bdr; j < resultWidth-bdr; j++) {
+				
+				// for each pixel, apply this filter:
+				for(int p = i-bdr; p <= i+bdr; p++) {
+					for(int q = j-bdr; q <= j+bdr; q++) {
+						sum += getGrayValue(result.getRGB(q, p));
+					}
+				}
+				
+				avg = sum / (n*n);
+				avg = getRGBValue(avg);
+				newResult.setRGB(jj, ii, avg);
+				jj++;
+				sum = 0;				
+			}
+			ii++;
+			jj=0;
+		}
+		
+		return newResult;
+	}
+	
+	private static BufferedImage medianAndCrop(BufferedImage result) {
+		
+		int ii = 0;
+		int jj = 0;
+		BufferedImage newResult = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_BYTE_GRAY);
+		
+		int resultHeight = result.getHeight();
+		int resultWidth = result.getWidth();
+		
+		ArrayList<Integer> neighbors = new ArrayList<Integer>();
+		int mid = 0;
+		
+		for(int i = bdr; i < resultHeight-bdr; i++) {
+			for(int j = bdr; j < resultWidth-bdr; j++) {
+				
+				// for each pixel, apply this filter:
+				for(int p = i-bdr; p <= i+bdr; p++) {
+					for(int q = j-bdr; q <= j+bdr; q++) {
+						neighbors.add(getGrayValue(result.getRGB(q, p)));
+					}
+				}
+				
+				Collections.sort(neighbors);
+				mid = neighbors.size() / 2;  // mid = the index of the arraylist that contains the rgb value
+				mid = neighbors.get(mid); // mid = the gray value in the middle of the neighbors
+				mid = getRGBValue(mid);
+				newResult.setRGB(jj, ii, mid);
+				jj++;
+				neighbors.clear();				
+			}
+			ii++;
+			jj=0;
+		}
+		
+		return newResult;
+	}
+	
+	private static BufferedImage addReflectivePadding(BufferedImage result) {
+		int resultHeight = result.getHeight();
+		int resultWidth = result.getWidth();
+		int imageRGB;
+		
+		///// top-left corner
+		int ii = bdr-1;
+		int jj = bdr-1;
+		for(int i = 0; i < bdr; i++) {
+			for(int j = 0; j < bdr; j++) {
+				imageRGB = image.getRGB(jj,ii);
+				result.setRGB(j, i, imageRGB);
+				jj--;
+			}
+			ii--;
+			jj=bdr-1;
+		}
+		
+		///// top-center
+		ii = bdr-1;
+		jj = 0;
+		for(int i = 0; i < bdr; i++) {
+			for(int j = bdr; j < resultWidth-bdr; j++) {
+				imageRGB = image.getRGB(jj,ii);
+				result.setRGB(j, i, imageRGB);
+				jj++;
+			}
+			ii--;
+			jj=0;
+		}
+		
+		///// top-right corner
+		ii = bdr-1;
+		jj = imageWidth-1;
+		for(int i = 0; i < bdr; i++) {
+			for(int j = resultWidth-bdr; j < resultWidth; j++) {
+				imageRGB = image.getRGB(jj,ii);
+				result.setRGB(j, i, imageRGB);
+				jj--;
+			}
+			ii--;
+			jj = imageWidth-1;
+		}
+		
+		///// left edge
+		ii = 0;
+		jj = bdr-1;
+		for(int i = bdr; i < resultHeight-bdr; i++) {
+			for(int j = 0; j < bdr; j++) {
+				imageRGB = image.getRGB(jj,ii);
+				result.setRGB(j, i, imageRGB);
+				jj--;
+			}
+			ii++;
+			jj=bdr-1;
+		}
+		
+		///// bottom-left corner
+		ii = imageHeight-1;
+		jj = bdr-1;
+		for(int i = resultHeight-bdr; i < resultHeight; i++) {
+			for(int j = 0; j < bdr; j++) {
+				imageRGB = image.getRGB(jj,ii);
+				result.setRGB(j, i, imageRGB);
+				jj--;
+			}
+			ii--;
+			jj = bdr-1;
+		}
+		
+		///// bottom-center
+		ii = imageHeight-1;
+		jj = 0;
+		for(int i = resultHeight-bdr; i < resultHeight; i++) {
+			for(int j = bdr; j < resultWidth-bdr; j++) {
+				imageRGB = image.getRGB(jj,ii);
+				result.setRGB(j, i, imageRGB);
+				jj++;
+			}
+			ii--;
+			jj=0;
+		}
+		
+		///// right edge
+		ii = 0;
+		jj = imageWidth-1;
+		for(int i = bdr; i < resultHeight-bdr; i++) {
+			for(int j = resultWidth-bdr; j < resultWidth; j++) {
+				imageRGB = image.getRGB(jj,ii);
+				result.setRGB(j, i, imageRGB);
+				jj--;
+			}
+			ii++;
+			jj=imageWidth-1;
+		}
+		
+		///// bottom-right corner
+		ii = imageHeight-1;
+		jj = imageWidth-1;
+		for(int i = resultHeight-bdr; i < resultHeight; i++) {
+			for(int j = resultWidth-bdr; j < resultWidth; j++) {
+				imageRGB = image.getRGB(jj,ii);
+				result.setRGB(j, i, imageRGB);
+				jj--;
+			}
+			ii--;
+			jj = imageWidth-1;
+		}
+		return result;
+	}
+	
+	private static BufferedImage zeroImagePadding(BufferedImage result) {
+		int ii = 0;
+		int jj = 0;
+		int resultHeight = result.getHeight();
+		int resultWidth = result.getWidth();
+		for(int i = bdr; i < resultHeight-bdr; i++) {
+			for(int j = bdr; j < resultWidth-bdr; j++) {
+				int imageRGB = image.getRGB(jj, ii);
+				result.setRGB(j, i, imageRGB);
+				jj++;
+			}
+			ii++;
+			jj=0;
+		}
+		return result;
+	}
+	
+	public BufferedImage smoothingFilter(int filterSize) {
+		System.out.println("Smoothing filter in progress...");
+		setFilterSize(filterSize);
+		int resultHeight = imageHeight+(bdr*2);
+		int resultWidth = imageWidth+(bdr*2);
+		BufferedImage result = new BufferedImage(resultWidth, resultHeight, BufferedImage.TYPE_BYTE_GRAY);
+		
+		result = zeroImagePadding(result);
+		result = addReflectivePadding(result);
+		result = smoothAndCrop(result);
+		return result;
+	}
+	
+	public BufferedImage medianFilter(int filterSize) {
+		System.out.println("Median filter in progress...");
+		setFilterSize(filterSize);
+		int resultHeight = imageHeight+(bdr*2);
+		int resultWidth = imageWidth+(bdr*2);
+		BufferedImage result = new BufferedImage(resultWidth, resultHeight, BufferedImage.TYPE_BYTE_GRAY);
+		
+		result = zeroImagePadding(result);
+		result = addReflectivePadding(result);
+		result = medianAndCrop(result);
+		return result;
+	}
+	
+	private BufferedImage laplacianAndCrop(BufferedImage result) {
+		System.out.println("Laplacian filter in progress...");
+		int resultHeight = result.getHeight();
+		int resultWidth = result.getWidth();
+		
+		BufferedImage newResult = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_BYTE_GRAY);
+		
+		int r = 0;
+		int l = 0;
+		int u = 0;
+		int d = 0;
+		int f = 0;
+		int finalRGB = 0;
+		int ii = 0;
+		int jj = 0;
+		
+		for(int i = bdr; i < resultHeight-bdr; i++) {
+			for(int j = bdr; j < resultWidth-bdr; j++) {
+				
+				r = getGrayValue(result.getRGB(j+1, i));
+				l = getGrayValue(result.getRGB(j-1, i));
+				u = getGrayValue(result.getRGB(j, i+1));
+				d = getGrayValue(result.getRGB(j, i-1));
+				f = getGrayValue(result.getRGB(j, i));
+				finalRGB = getRGBValue((r + l + u + d) - (4 * f)); 
+				newResult.setRGB(jj, ii, finalRGB);				
+				jj++;
+			}
+			ii++;
+			jj=0;
+		}
+		
+		return newResult;
+	}
+	
+	public BufferedImage laplacianFilter() {
+		System.out.println("Laplacian filter in progress...");
+		setFilterSize(3);
+		int resultHeight = imageHeight+(bdr*2);
+		int resultWidth = imageWidth+(bdr*2);
+		BufferedImage result = new BufferedImage(resultWidth, resultHeight, BufferedImage.TYPE_BYTE_GRAY);
+		
+		result = zeroImagePadding(result);
+		result = addReflectivePadding(result);
+		result = laplacianAndCrop(result);
+		return result;
+	}
+	
+	public BufferedImage highboostFilter(int A) {
+		System.out.println("High-boost filter in progress...");
+		setFilterSize(3);
+		int resultHeight = imageHeight+(bdr*2);
+		int resultWidth = imageWidth+(bdr*2);
+		BufferedImage result = new BufferedImage(resultWidth, resultHeight, BufferedImage.TYPE_BYTE_GRAY);
+		
+		result = zeroImagePadding(result);
+		result = addReflectivePadding(result);
+		result = laplacianAndCrop(result);
+		
+		BufferedImage i = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_BYTE_GRAY);		
+		i = copyImage(image, i);
+		
+		i = multiplyImageByConstant(i, A);
+		result = addImages(i, result);
+		return result;
+	}
+	
+	private static BufferedImage copyImage(BufferedImage toCopy, BufferedImage result) {if (toCopy.getHeight() == result.getHeight() && toCopy.getWidth() == result.getWidth()) {
+			System.out.println("Images are the same height!");
+			int height = toCopy.getHeight();
+			int width = toCopy.getWidth();
+			
+			for(int i = 0; i < height; i++) {
+				for(int j = 0; j < width; j++) {
+					result.setRGB(j, i, toCopy.getRGB(j, i));
+				}
+			}
+			return result;
+		}
+		else {
+			System.out.println("Dimensions do not match!");
+			return toCopy;
+		}
+	}
+	
+	private static BufferedImage addImages(BufferedImage image1, BufferedImage image2) {
+		
+		if (image1.getHeight() == image2.getHeight() && image1.getWidth() == image2.getWidth()) {
+			System.out.println("Images are the same height!");
+			int height = image1.getHeight();
+			int width = image1.getWidth();
+			BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
+						
+			int rgb1 = 0;
+			int rgb2 = 0;
+			int resultRGB = 0;
+			
+			for(int i = 0; i < height; i++) {
+				for(int j = 0; j < width; j++) {
+					
+					rgb1 = getGrayValue(image1.getRGB(j,i));
+					rgb2 = getGrayValue(image2.getRGB(j,i));
+					resultRGB = getRGBValue(rgb1 + rgb2);
+					
+					result.setRGB(j, i, resultRGB);
+					
+				}
+			}
+			return result;
+		}
+		
+		else {
+			System.out.println("Dimensions do not match!");
+			return image;
+		}
+	}
+	
+	private static BufferedImage multiplyImageByConstant(BufferedImage result, int A) {
+		
+		int height = result.getHeight();
+		int width = result.getWidth();
+		
+		int value = 0;
+		
+		for(int i = 0; i < height; i++) {
+			for(int j = 0; j < width; j++) {
+				
+				value = getGrayValue(result.getRGB(j, i));
+				value = value * (A-1);
+				value = getRGBValue(value);
+				result.setRGB(j, i, value);
+				
+			}
+		}
+		
+		return result;
+	}
+	
+	
+	
+	
+	
+	private static int getGrayValue(int rgb) {
+		Color c = new Color(rgb);
+		int gray = c.getRed();
+		return gray;
+	}
+	
+	private static int getRGBValue(int gray) {
+		int r = gray;
+		int g = gray;
+		int b = gray;
+		int col = (r << 16) | (g << 8) | b;
+		return col;
+	}
+	
+
+}
---
+++ src/edu/cpp/cs555/hw/ImagingGui.java
@@ -0,0 +1,450 @@
+package edu.cpp.cs555.hw;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.GridLayout;
+
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JComboBox;
+
+import edu.cpp.cs555.hw.ImageProcessor;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImagingGui extends JFrame {
+
+	private JPanel contentPane;
+	private File file;
+	private BufferedImage myImage;
+
+	/**
+	 * Launch the application.
+	 */
+	
+	//////////////////////////////////  t e s t   a r e a  //////////////////////////////////
+	
+	static int n = 5;
+	static int bdr = n/2;
+	static int imageHeight = 9;
+	static int imageWidth = 17;
+	
+	static int[][] image = new int[imageHeight][imageWidth];
+	
+	private static void populateArray() {
+		for(int i = 0; i < imageHeight; i++) { // up&down
+			for(int j = 0; j < imageWidth; j++) { // across
+				image[i][j] = (int) (Math.random()*10);
+			}
+		}
+	}
+	
+	private static void printArray(int[][] array) {
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array[0].length; j++) {
+				System.out.print(array[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
+	private static void imagePadding(int[][] result) {
+		int ii = 0;
+		int jj = 0;
+		int resultHeight = result.length;
+		int resultWidth = result[0].length;
+		for(int i = bdr; i < resultHeight-bdr; i++) {
+			for(int j = bdr; j < resultWidth-bdr; j++) {
+				result[i][j] = image[ii][jj];
+				jj++;
+			}
+			ii++;
+			jj=0;
+		}
+	}
+	
+	private static void reflectPadding(int[][] result) {
+		int resultHeight = result.length;
+		int resultWidth = result[0].length;
+		
+		///// top-left corner
+		int ii = bdr-1;
+		int jj = bdr-1;
+		for(int i = 0; i < bdr; i++) {
+			for(int j = 0; j < bdr; j++) {
+				result[i][j] = image[ii][jj];
+				jj--;
+			}
+			ii--;
+			jj=bdr-1;
+		}
+		
+		///// top-center
+		ii = bdr-1;
+		jj = 0;
+		for(int i = 0; i < bdr; i++) {
+			for(int j = bdr; j < resultWidth-bdr; j++) {
+				result[i][j] = image[ii][jj];
+				jj++;
+			}
+			ii--;
+			jj=0;
+		}
+		
+		///// top-right corner
+		ii = bdr-1;
+		jj = imageWidth-1;
+		for(int i = 0; i < bdr; i++) {
+			for(int j = resultWidth-bdr; j < resultWidth; j++) {
+				result[i][j] = image[ii][jj];
+				jj--;
+			}
+			ii--;
+			jj = imageWidth-1;
+		}
+		
+		///// left edge
+		ii = 0;
+		jj = bdr-1;
+		for(int i = bdr; i < resultHeight-bdr; i++) {
+			for(int j = 0; j < bdr; j++) {
+				result[i][j] = image[ii][jj];
+				jj--;
+			}
+			ii++;
+			jj=bdr-1;
+		}
+		
+		///// bottom-left corner
+		ii = imageHeight-1;
+		jj = bdr-1;
+		for(int i = resultHeight-bdr; i < resultHeight; i++) {
+			for(int j = 0; j < bdr; j++) {
+				result[i][j] = image[ii][jj];
+				jj--;
+			}
+			ii--;
+			jj = bdr-1;
+		}
+		
+		///// bottom-center
+		ii = imageHeight-1;
+		jj = 0;
+		for(int i = resultHeight-bdr; i < resultHeight; i++) {
+			for(int j = bdr; j < resultWidth-bdr; j++) {
+				result[i][j] = image[ii][jj];
+				jj++;
+			}
+			ii--;
+			jj=0;
+		}
+		
+		///// right edge
+		ii = 0;
+		jj = imageWidth-1;
+		for(int i = bdr; i < resultHeight-bdr; i++) {
+			for(int j = resultWidth-bdr; j < resultWidth; j++) {
+				result[i][j] = image[ii][jj];
+				jj--;
+			}
+			ii++;
+			jj=imageWidth-1;
+		}
+		
+		///// bottom-right corner
+		ii = imageHeight-1;
+		jj = imageWidth-1;
+		for(int i = resultHeight-bdr; i < resultHeight; i++) {
+			for(int j = resultWidth-bdr; j < resultWidth; j++) {
+				result[i][j] = image[ii][jj];
+				jj--;
+			}
+			ii--;
+			jj = imageWidth-1;
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		/**
+		
+		populateArray();
+		printArray(image);
+		
+		int resultHeight = imageHeight+(bdr*2);
+		int resultWidth = imageWidth+(bdr*2);
+		
+		System.out.println();
+		 
+		int[][] result = new int[resultHeight][resultWidth];
+		imagePadding(result);
+		printArray(result);
+		
+		System.out.println();
+		System.out.println();
+		
+		reflectPadding(result);
+		System.out.println("Printing with reflective padding:");
+		printArray(result);
+		
+		System.out.println();
+		
+		int ii = 0;
+		int jj = 0;
+		int[][] newResult = new int[imageHeight][imageWidth];
+		
+		int sum = 0;
+		int avg = 0;
+		
+		for(int i = bdr; i < resultHeight-bdr; i++) {
+			for(int j = bdr; j < resultWidth-bdr; j++) {
+				
+				// for each pixel, apply this filter:
+				for(int p = i-bdr; p <= i+bdr; p++) {
+					for(int q = j-bdr; q <= j+bdr; q++) {
+						sum += result[p][q];
+					}
+				}
+				
+				avg = sum / (n*n);
+				newResult[ii][jj] = avg;
+				jj++;
+				sum = 0;				
+			}
+			ii++;
+			jj=0;
+		}
+
+		System.out.println("Printing to test inner area:");
+		printArray(newResult);
+		
+		*/
+		
+		
+		////////////////////////////////// t e s t   a r e a  //////////////////////////////////	
+		
+		
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ImagingGui frame = new ImagingGui();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public ImagingGui() {
+		setTitle("Image Processing");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 230);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblInputImagePlease = new JLabel("Input Image: Please select a jpeg image to process");
+		lblInputImagePlease.setBounds(12, 13, 408, 15);
+		lblInputImagePlease.setHorizontalAlignment(SwingConstants.CENTER);
+		lblInputImagePlease.setFont(new Font("Tahoma", Font.BOLD, 12));
+		contentPane.add(lblInputImagePlease);
+		
+		JLabel lblNoFileSelected = new JLabel("No file selected");
+		lblNoFileSelected.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNoFileSelected.setBounds(12, 48, 236, 16);
+		contentPane.add(lblNoFileSelected);
+		
+		JButton btnBrowse = new JButton("Browse");
+		btnBrowse.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				JFileChooser chooser = new JFileChooser();
+			    FileNameExtensionFilter filter = new FileNameExtensionFilter(
+			        "JPG & GIF Images", "jpg", "gif");
+			    chooser.setFileFilter(filter);
+			    int returnVal = chooser.showOpenDialog(null);
+			    if(returnVal == JFileChooser.APPROVE_OPTION) {
+			    	file = chooser.getSelectedFile();
+			    	System.out.println("You chose to open this file: " +
+			            file.getName());
+			    	lblNoFileSelected.setText(file.getName());
+			    	myImage = loadColorImage(file);
+			    	myImage = convert2Gray(myImage);
+			    }
+			 
+			}
+			
+			private BufferedImage convert2Gray(BufferedImage image) {
+				BufferedImage grayImg = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_BYTE_GRAY);
+				for(int i = 0; i < image.getHeight(); i++) {
+					for(int j = 0; j < image.getWidth(); j++) {
+						int value = image.getRGB(j, i);
+						grayImg.setRGB(j, i, value);
+					}
+				}
+				return grayImg;
+			}
+			
+			
+			/**
+			 * File IO for the image */
+			private BufferedImage loadColorImage(File file) {
+				BufferedImage img = null;
+				try {
+					img = ImageIO.read(file.getAbsoluteFile());
+				} catch (IOException e) {
+					System.out.println("The image was unable to load. ");
+				}
+				return img;
+			}
+		});
+		btnBrowse.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnBrowse.setBounds(260, 44, 75, 25);
+		contentPane.add(btnBrowse);
+		
+		JButton btnDisplay = new JButton("Display");
+		btnDisplay.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				JPanel imagePanel = new JPanel();
+				JFrame displayInputImage = new JFrame("Input");
+				ImageIcon inputImage = new ImageIcon(myImage/*file.getAbsolutePath()*/);
+				imagePanel.add(new JLabel(inputImage));
+				displayInputImage.getContentPane().add(imagePanel);
+				displayInputImage.pack();
+				
+				/* Input Image Window Setup */
+				displayInputImage.setVisible(true);
+				setDefaultCloseOperation(EXIT_ON_CLOSE);
+				
+			}
+		});
+		btnDisplay.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		btnDisplay.setBounds(347, 44, 73, 25);
+		contentPane.add(btnDisplay);
+		
+		JLabel lblProcessPleaseSelect = new JLabel("Process: Please select a process from the dropdown menu");
+		lblProcessPleaseSelect.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblProcessPleaseSelect.setHorizontalAlignment(SwingConstants.CENTER);
+		lblProcessPleaseSelect.setBounds(12, 94, 408, 16);
+		contentPane.add(lblProcessPleaseSelect);
+		
+		String[] options = {
+				/*0*/"============= Spatial Filtering =============",
+				/*1*/"Smoothing Filter",
+				/*2*/"Median Filter",
+				/*3*/"Sharpening Laplacian filter",
+				/*4*/"High-boosting filter"};
+		JComboBox comboBox = new JComboBox(options);
+		comboBox.setBounds(12, 123, 323, 22);
+		contentPane.add(comboBox);
+		
+		JButton btnProcess = new JButton("Go!");
+		btnProcess.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				ImageProcessor ip = new ImageProcessor(myImage);
+				BufferedImage result = myImage;
+				
+				int process = comboBox.getSelectedIndex();
+				switch(process) {
+					case 0: { // no action
+						break;
+					}
+					case 1: { // smoothing filter
+						String filterSize = JOptionPane.showInputDialog(null, "Please enter the size of your filter (odd numbers only)");
+						result = ip.smoothingFilter(Integer.parseInt(filterSize));
+						createOutputWindow(result);
+						break;
+					}
+					case 2: { // median filter
+						String filterSize = JOptionPane.showInputDialog(null, "Please enter the size of your filter (odd numbers only)");
+						result = ip.medianFilter(Integer.parseInt(filterSize));
+						createOutputWindow(result);
+						break;
+					}
+					case 3: { // sharpening laplacian filter
+						result = ip.laplacianFilter();
+						createOutputWindow(result);
+						break;
+					}
+					case 4: { // high-boost filter
+						String A = JOptionPane.showInputDialog(null, "Please enter a value for coefficient A (A >= 1)");
+						result = ip.highboostFilter(Integer.parseInt(A));
+						createOutputWindow(result);
+						break;
+					}
+					default: {
+						break;	
+					}
+				}
+				
+			}
+			
+			/**
+			 * The GUI for displaying an image.
+			 * @param myImage
+			 */
+			private void createOutputWindow(BufferedImage myImage)
+			{
+				/* Create a new window */
+				JFrame displayInputImage = new JFrame("Output");
+				
+				/* Create a new image panel */
+				JPanel imagePanel = new JPanel();
+
+				/* Load display image as an icon */
+				ImageIcon inputImage = new ImageIcon(myImage);
+				
+				/* Add icon to image panel as a JLabel */
+				imagePanel.add(new JLabel(inputImage));
+				
+				displayInputImage.add(imagePanel);
+				displayInputImage.pack();
+				
+				/* Input Image Window Setup */
+				displayInputImage.setVisible(true);
+				setDefaultCloseOperation(EXIT_ON_CLOSE);
+			}
+			
+		});
+		btnProcess.setBounds(347, 123, 73, 25);
+		contentPane.add(btnProcess);
+		
+		JLabel lblKhamilleS = new JLabel("Khamille S - Digital Image Processing - Raheja cs555");
+		lblKhamilleS.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		lblKhamilleS.setHorizontalAlignment(SwingConstants.CENTER);
+		lblKhamilleS.setBounds(12, 158, 408, 16);
+		contentPane.add(lblKhamilleS);
+	}
+}
